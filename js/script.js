f701e04e0cf47fcc7718287d2f62e99f218cb70b
Povrty <iakwan@s.sfusd.edu>
2019-12-17 02:03:06 +0000

    Need to finish win() function

--- js/script.js
+++ js/script.js
@@ -1,82 +1,86 @@
 let counterX = 0;
 let counterO = 0;
 let text = "x";
+let win = [10, 26, 31, 33, 36, 41, 59];
+
+function win(value, array){
+    if ($.inArray(counterX, win) != -1){
+        
+    }
+}
 
 function performLogic(button, tile){
     $(button).hide();
     $(tile).text(text);
 }
-function cycle() {
-    if (text == "o") {
+function cycle(number) {
+    if (text === "o") {
         text = "x";
-        counterX = counterX + 1;
-    } else if (text == "x") {
+        counterO = counterO + number;
+    } else if (text === "x") {
         text = "o";
-        counterO = counterO + 1;
+        counterX = counterX + number;
     }
 }
 
-$("#button1").click(function() {
-    cycle();
-    performLogic("#button1","#tile1");
+function consoleLog(){
     console.log(counterO);
     console.log(counterX);
     console.log(text);
+}
+
+$("#button1").click(function() {
+    cycle(2);
+    performLogic("#button1","#tile1");
+    consoleLog();
+    win();
 });
 
 $("#button2").click(function() {
-    cycle();
+    cycle(3);
     performLogic("#button2","#tile2");
-    console.log(counter);
-    console.log(text);
+    consoleLog();
+    win();
 });
 
 $("#button3").click(function() {
-    cycle();
+    cycle(5);
     performLogic("#button3","#tile3");
-    console.log(counter);
-    console.log(text);
+    consoleLog();
 });
 
 $("#button4").click(function() {
-    cycle();
+    cycle(7);
     performLogic("#button4","#tile4");
-    console.log(counter);
-    console.log(text);
+    consoleLog();
 });
 
 $("#button5").click(function() {
-    cycle();
+    cycle(11);
     performLogic("#button5","#tile5");
-    console.log(counter);
-    console.log(text);
+    consoleLog();
 });
 
 $("#button6").click(function() {
-    cycle();
+    cycle(13);
     performLogic("#button6","#tile6");
-    console.log(counter);
-    console.log(text);
+    consoleLog();
 });
 
 $("#button7").click(function() {
-    cycle();
+    cycle(17);
     performLogic("#button7","#tile7");
-    console.log(counter);
-    console.log(text);
+    consoleLog();
 });
 
 $("#button8").click(function() {
-    cycle();
+    cycle(19);
     performLogic("#button8","#tile8");
-    console.log(counter);
-    console.log(text);
+    consoleLog();
 });
 
 $("#button9").click(function() {
-    cycle();
+    cycle(23);
     performLogic("#button9","#tile9");
-    console.log(counter);
-    console.log(text);
+    consoleLog();
 });
-
