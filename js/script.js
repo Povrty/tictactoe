d72ceae7fec18e51d363e78768fea4cc93c4d3c8
Povrty <iakwan@s.sfusd.edu>
2020-02-06 07:48:24 +0000

    Need to fix Win

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let counterX = 0;
 let counterO = 0;
 let text = "x";
-let win = [10, 26, 31, 33, 36, 41, 59];
+let win = [10, 26, 27, 31, 33, 36, 41, 43, 49, 58, 59, 61, 64];
 
 function performLogic(button, tile){
     $(button).hide();
@@ -22,57 +22,88 @@ function consoleLog(){
     console.log(counterX);
     console.log(text);
 }
+function winner(variable) {
+    if (win.includes(variable)) {
+        console.log("true");
+        alert(text + " is the Winner!")
+        location.reload();
+    } else {
+        console.log("false");
+    }
+}
+
+$(".restart").click(function() {
+    location.reload();
+})
 
 $("#button1").click(function() {
     cycle(2);
     performLogic("#button1","#tile1");
     consoleLog();
+    winner(counterX);
+    winner(counterO);
 });
 
 $("#button2").click(function() {
     cycle(3);
     performLogic("#button2","#tile2");
     consoleLog();
+    winner(counterX);
+    winner(counterO);
 });
 
 $("#button3").click(function() {
     cycle(5);
     performLogic("#button3","#tile3");
     consoleLog();
+    winner(counterX);
+    winner(counterO);
 });
 
 $("#button4").click(function() {
     cycle(7);
     performLogic("#button4","#tile4");
     consoleLog();
+    winner(counterX);
+    winner(counterO);
 });
 
 $("#button5").click(function() {
     cycle(11);
     performLogic("#button5","#tile5");
     consoleLog();
+    winner(counterX);
+    winner(counterO);
 });
 
 $("#button6").click(function() {
     cycle(13);
     performLogic("#button6","#tile6");
     consoleLog();
+    winner(counterX);
+    winner(counterO);
 });
 
 $("#button7").click(function() {
     cycle(17);
     performLogic("#button7","#tile7");
     consoleLog();
+    winner(counterX);
+    winner(counterO);
 });
 
 $("#button8").click(function() {
     cycle(19);
     performLogic("#button8","#tile8");
     consoleLog();
+    winner(counterX);
+    winner(counterO);
 });
 
 $("#button9").click(function() {
     cycle(23);
     performLogic("#button9","#tile9");
     consoleLog();
+    winner(counterX);
+    winner(counterO);
 });
